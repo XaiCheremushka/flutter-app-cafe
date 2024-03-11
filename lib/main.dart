9e2eacd0241f19a63008ca4f4cbe6686615c252b
Xai_Cheremushka <vania55ru@mail.ru>
2024-03-11 21:09:42 +0600

    .

--- lib/main.dart
+++ lib/main.dart
@@ -5,12 +5,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_course/src/app.dart';
 import 'package:flutter_course/src/features/menu/view/menu_screen.dart';
 
-// void main() {
-//   runZonedGuarded(() => runApp(const CoffeeShopApp()), (error, stack) {
-//     log(error.toString(), name: 'App Error', stackTrace: stack);
-//   });
-// }
-
 void main() {
-  runApp(MenuScreen());
+  runZonedGuarded(() => runApp(const CoffeeShopApp()), (error, stack) {
+    log(error.toString(), name: 'App Error', stackTrace: stack);
+  });
 }
+
+// void main() {
+//   runApp(MenuScreen());
+// }
--- lib/src/app.dart
+++ lib/src/app.dart
@@ -3,6 +3,8 @@ import 'package:flutter_course/src/theme/theme.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'package:flutter_course/src/features/menu/view/menu_screen.dart';
+
 class CoffeeShopApp extends StatelessWidget {
   const CoffeeShopApp({super.key});
 
@@ -18,7 +20,7 @@ class CoffeeShopApp extends StatelessWidget {
       supportedLocales: AppLocalizations.supportedLocales,
       onGenerateTitle: (context) => AppLocalizations.of(context)!.title,
       theme: theme,
-      home: const Center(),
+      home: MenuScreen(),
     );
   }
 }
